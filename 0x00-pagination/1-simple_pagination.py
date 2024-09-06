39e45944f17c319db3178bbbce7b9d3162a854bf
Boniface Murangiri <bonnyrangi95@gmail.com>
2024-09-06 15:49:35 +0300

    Remove trailling white spaces

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,10 +8,13 @@ from typing import List, Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """Server class to paginate a database of popular baby names.
+    """
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Iniatialize dataset attribute to None."""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -28,8 +31,8 @@ class Server:
         assert type(page) == int and page > 0
         assert type(page_size) == int and page_size > 0
         data = self.dataset()
-        indicies = index_range(page, page_size)
-        return data[indicies[0]:indicies[1]]
+        indices = index_range(page, page_size)
+        return data[indices[0]:indices[1]]
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
